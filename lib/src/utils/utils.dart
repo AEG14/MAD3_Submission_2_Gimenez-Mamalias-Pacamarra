619435fbefa32bbe3c646dd13bb5fa3817eccc0c
AnthonyPacamarra <anthonygwapopacamarra@gmail.com>
2024-06-22 23:47:30 +0800

    added utils.dart

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:state_change_demo/src/models/post.model.dart';
+import 'package:state_change_demo/src/models/user.model.dart';
+
 
 class PostController with ChangeNotifier {
   Map<String, dynamic> posts = {};
@@ -166,6 +168,43 @@ class PostController with ChangeNotifier {
       working = false;
       notifyListeners();
     }
+  }
+}
+
+class UserController with ChangeNotifier {
+  Map<String, dynamic> users = {};
+  bool working = true;
+  Object? error;
+
+  List<User> get userList => users.values.whereType<User>().toList();
+
+  getUsers() async {
+    try {
+      working = true;
+      List result = [];
+      http.Response res = await HttpService.get(
+          url: "https://jsonplaceholder.typicode.com/users");
+      if (res.statusCode != 200 && res.statusCode != 201) {
+        throw Exception("${res.statusCode} | ${res.body}");
+      }
+      result = jsonDecode(res.body);
+
+      List<User> tmpUser = result.map((e) => User.fromJson(e)).toList();
+      users = {for (User u in tmpUser) "${u.id}": u};
+      working = false;
+      notifyListeners();
+    } catch (e, st) {
+      print(e);
+      print(st);
+      error = e;
+      working = false;
+      notifyListeners();
+    }
+  }
+
+  clear() {
+    users = {};
+    notifyListeners();
   }
 }
 
